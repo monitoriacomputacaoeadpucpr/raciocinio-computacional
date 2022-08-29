bd666133d2720a9d7a46ed6e9abfb836d044ba3e
LyeneSouza <lyenesb@gmail.com>
2022-08-29 18:17:12 -0300

    Código do vídeo sobre debug

---
+++ Semana 5/codigo-de-videos/monitora-lyene/debug.py
@@ -0,0 +1,27 @@
+string = 'Hello'
+inteiro = 20
+float = 2.5
+lista = ['a', 'b', 'c']
+
+print('Hello world!')
+print('Olá mundo!')
+print('Hola mundo!')
+print('Bonjour le monde!')
+
+
+numero = 5
+if numero < 5:
+    print('Menor')
+elif numero == 5:
+    print('Igual')
+else:
+    print('Maior')
+print('Fim')
+
+for i in range(5):
+    print(i)
+print('Fim')
+
+lista = ['a', 'b', 'c']
+for i in range(len(lista)):
+    print(lista[i])
