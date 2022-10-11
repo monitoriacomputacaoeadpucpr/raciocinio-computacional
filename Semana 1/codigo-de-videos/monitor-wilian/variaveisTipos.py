fda2ffe31f82964f9a1e4311e40a527054af09a4
Wilian Krinke <krinkewilian@gmail.com>
2022-10-10 21:31:05 -0300

    insercao de codigos de videos

---
+++ Semana 1/codigo-de-videos/monitor-wilian/variaveisTipos.py
@@ -0,0 +1,29 @@
+# Lógica de programação é a técnica de encadear pensamentos para atingir determinado
+# objetivo.(Moraes,2000)
+
+# Algoritmo é uma sequencia logica de instruções, em alguma linguagem de programação ou pseudo-código, para resolver alguma tarefa
+
+numero = 12.3333  # atribuição de variavel
+frase = "Logica de programação é legal"
+booleano = True
+
+# variavel é um endereço na memoria do computador que é destinada ao armazenamento de dados que serão recuperados posteriormente por algum motivo
+
+# tipos primitivos de variaveis são pilares fundamentais para estrutura de qualquer linguagem de programação, dependendo do tipo da variavel pode consumir mais ou menos recursos(espaço) na memoria
+
+# saida de dados com a função print no python
+
+print(numero)
+print(frase)
+print(type(booleano))
+
+
+# numero1 = int(input("Digite o primeiro numero: "))
+# numero2 = float(input('Digite o segundo numero: '))
+
+
+# print(f"A soma dos numeros é: {numero1 + numero2}")
+
+
+print(f'{numero:.4f}')
+print('O numero: {}, {}'.format(numero, frase))
