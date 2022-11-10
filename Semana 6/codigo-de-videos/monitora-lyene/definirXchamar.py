2ffb26fbc2304edab1b344c45ee4b01a67afbdcf
Lyene <lyene.benvenutti@southsystem.com.br>
2022-11-10 14:13:28 -0300

    Código do vídeo Definir versus Chamar uma função

---
+++ Semana 6/codigo-de-videos/monitora-lyene/definirXchamar.py
@@ -0,0 +1,16 @@
+# Imprime "Miau" 3 vezes
+for i in range(3):
+    print('Miau')
+
+#Define uma função para imprimir "Miau" 3 vezes
+def printMiau():
+    """
+    A função printMiau() imprime "Miau" na tela 3 vezes
+    """
+
+    for i in range(3):
+        print('Miau')
+
+
+# Chamando a função
+printMiau()
